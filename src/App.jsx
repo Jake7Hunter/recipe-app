d3d549207745930b5c5fa6ecb6ed41a4259843e5
Jake Hunter <jake7hunter@hmail.com>
2022-04-19 15:52:54 -0400

    Added pages to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-function App(){
-    return(
-        <h1>Hi</h1>
-    )
+import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Category />
+        <Pages />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,5 @@
+function Pages() {
+    return <h1>Hi</h1>
+}
+
+export default Pages;
